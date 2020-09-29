5a891e4ba5e13b7ee4e28180d7080d3c1aef7550
Caio Faustino <caio.faustino@adyen.com>
2020-09-29 15:41:33 +0200

    Make release task to publish to bintray.

--- app/src/main/java/dev/caiofaustino/githubactionstest/MainActivity.kt
+++ app/src/main/java/dev/caiofaustino/githubactionstest/MainActivity.kt
@@ -2,10 +2,16 @@ package dev.caiofaustino.githubactionstest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dev.caiofaustino.testlib.TestClass
 
 class MainActivity : AppCompatActivity() {
+
+    private val testClass = TestClass()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        testClass.logMe()
     }
 }
